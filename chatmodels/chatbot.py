b107341aff881625bb00197347f54b99bdff6334
mohit gaikwad <mohitgaikwad2911@gmail.com>
2026-07-15 19:12:19 +0530

    chatbot normal

--- chatmodels/chatbot.py
+++ chatmodels/chatbot.py
@@ -5,12 +5,17 @@ load_dotenv()
 
 model = init_chat_model("groq:llama-3.3-70b-versatile",temperature=0.9,max_tokens=50)
 
+messages=[
 
-print("-------Welcome to the Chatbot Enter 0 t exit chat-------")
+]
+
+print("-------Welcome to the Chatbot Enter 0 to exit chat-------")
 
 while True:
     prompt = input("You: ") 
+    messages.append(prompt)
     if prompt=="0":
         break
-    response = model.invoke(prompt)
+    response = model.invoke(messages)
+    messages.append(response.content)
     print("Bot: ", response.content)
